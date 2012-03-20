c66cb9335c6612e08120ad9fa21a01c91eef4bfe
Marcelo Coraça de Freitas <marcelo@umporvez.com.br>
2012-03-20 20:40:00 -0300

    ported to the new kowconfig API

--- src/apq_provider-postgresql.adb
+++ src/apq_provider-postgresql.adb
@@ -17,7 +17,7 @@ package body APQ_Provider.postgresql is
 
 	function Main_Factory is new APQ_Provider.Generic_Connection_Factory( Connection_Type => APQ.postgresql.Client.Connection_Type );
 
-	function Factory( C : in KOW_Config.Config_File ) return APQ.Connection_Ptr is
+	function Factory( C : in KOW_Config.Config_File_Type ) return APQ.Connection_Ptr is
 	begin
 		return Main_Factory( C );
 	end Factory;
--- src/apq_provider-postgresql.ads
+++ src/apq_provider-postgresql.ads
@@ -14,6 +14,6 @@ with APQ_Provider;
 package APQ_Provider.postgresql is
 
 
-	function Factory( C : in KOW_Config.Config_File ) return APQ.Connection_Ptr;
+	function Factory( C : in KOW_Config.Config_File_Type ) return APQ.Connection_Ptr;
 
 end APQ_Provider.postgresql;
